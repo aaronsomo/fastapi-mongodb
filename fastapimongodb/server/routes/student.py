b765eb60a3e074d53a00f53a0883c653d4d348f6
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2021-06-25 14:20:20 -0500

    added get requests for all students and for individual student by id

--- fastapimongodb/server/routes/student.py
+++ fastapimongodb/server/routes/student.py
@@ -17,6 +17,7 @@ from fastapimongodb.server.models.student import (
 
 router = APIRouter()
 
+
 @router.post("/", response_description="Student data added into the database")
 async def add_student_data(student: StudentSchema = Body(...)):
     student = jsonable_encoder(student)
@@ -33,3 +34,19 @@ async def add_student_data(student: StudentSchema = Body(...)):
     # }
 
     return ResponseModel(new_student, "Student added successfully.")
+
+
+@router.get("/", response_description="Students retrieved")
+async def get_students():
+    students = await retrieve_students()
+    if students:
+        return ResponseModel(students, "Students data retrieved successfully")
+    return ResponseModel(students, "Empty list returned")
+
+
+@router.get("/{id}", response_description="Student data retrieved successfully")
+async def get_student_data(id):
+    student = await retrieve_student(id)
+    if student:
+        return ResponseModel(student, "Student data retrieved successfully")
+    return ErrorResponseModel("An error occured.", 404, "Student doesn't exist.")
